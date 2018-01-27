d6b2c5d1b2debea490274f1360ebd73e24db745f
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-01-27 22:40:04 +0530

    implement arithmetic opn and app

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ class App {
     constructor() {
         this.mic = new Mic(this.micResponseHandler)
         this.nlp = setUpNlp()
+        this.handlerExecutor = run_handler
     }
 
     micResponseHandler(text) {
         console.log('response handler says', text)
+        let result = this.nlp.test(text)
+
     }
 
 }
