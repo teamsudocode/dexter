0f48f138f0cf8b7c45986c7c736a926eafc06de8
Souvik Sen <souviksen44@gmail.com>
2018-01-28 04:21:41 +0530

    Add python hacks

--- app.py
+++ app.py
@@ -3,18 +3,39 @@ from judge import runProcess
 
 app = Flask('dexter')
 
+
 @app.route('/compile', methods=['POST'])
 def compile():
     content = r'' + request.form['content']
+    # with open('my.py', 'w') as f:
+    #     print(exec(content))
     with open('intermediate.py', 'w') as f:
-        print(repr(content).replace("'",''))
-        f.write(repr(content).replace("",''))
+        # print(repr(content).replace("'", ''))
+        # new_content = content.replace("'", '')
+        for i in content:
+            f.write(i)
 
-    outBuf,errBuf = runProcess(['python3', 'intermediate.py'])
-    return jsonify({
-        'out': str(outBuf),
-        'err': str(errBuf)
-    })
+        # f.write(repr(content).replace("", ''))
+
+    outBuf, errBuf = runProcess(['python3', 'intermediate.py'])
+    return jsonify({'out': str(outBuf), 'err': str(errBuf)})
+
+
+@app.route('/js', methods=['POST'])
+def interpret():
+    content = r'' + request.form['content']
+    # with open('my.py', 'w') as f:
+    #     print(exec(content))
+    with open('intermediate.js', 'w') as f:
+        # print(repr(content).replace("'", ''))
+        # new_content = content.replace("'", '')
+        for i in content:
+            f.write(i)
+
+        # f.write(repr(content).replace("", ''))
+
+    outBuf, errBuf = runProcess(['node', 'intermediate.js'])
+    return jsonify({'out': str(outBuf), 'err': str(errBuf)})
 
 
 if __name__ == '__main__':
